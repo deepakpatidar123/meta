788e35ffb4b7a68c3f2b26a4d309efce03368e59
deepakpatidar123 <118708892+deepakpatidar123@users.noreply.github.com>
2022-11-25 12:56:30 +0530

    Add files via upload

---
+++ 4 app.js
@@ -0,0 +1,15 @@
+//GETELEMENTBYTAGNAME
+var li = document.getElementsByTagName('li');
+console.log(li);
+console.log(li[1]);
+li[1].textContent = 'Hello';
+li[1].style.fontWeight = 'bold';
+li[1].style.fontWeight = 'yellow';
+
+////Gives error
+// //items[1].style.backgroundColor = '#f4f4f4';
+
+for(var i=0;i<li.length;i++){
+ li[i].style.backgroundColor = '#f4f4f4';
+
+}
