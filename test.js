8606594633a84a85e11fa208781698d59ec61eaf
deepakpatidar123 <118708892+deepakpatidar123@users.noreply.github.com>
2023-01-02 15:50:34 +0530

    request and response

--- test.js
+++ test.js
@@ -1,13 +1,42 @@
 
-//server creation
-//1.http module
 
 const http=require('http');
-
-const server=http.createServer((req, res)=>{
+const server= http.createServer((req,res)=>{
+    res.end('deepak')
     console.log('deepak')
-})
+    // res.end('deepak')
+    // console.log('deepak')
 
-server.listen(4000, 'localhost',()=>{
-    console.log('server is listening on port 4000');
+
+    // res.setHeader('Content-Type','text/html');
+    // res.write('<html>');
+    // res.write('<head><title>My First Page</title></head>');
+    // res.write('<body><h1>Hello form my Node.js Server!</h1></body>');
+    // res.write('</html');
+    // res.end();
+
+
+    const url=req.url;
+    if(url==="/home"){
+        res.write("<html>");
+        res.write("<head><title>Home</title></head>");
+        res.write("<body><h1>Wellocome home</h1></body>");
+        res.write("</html>");
+        res.end();
+    } 
+    if(url==="/about"){
+        res.write("<html>");
+        res.write("<head><title>about</title></head>");
+        res.write("<body><h1>Welcome About Us page</h1></body>");
+        res.write("</html>");
+        res.end();
+    }
+    if(url==="/node"){
+        res.write("<html>");
+        res.write("<head><title>Node</title></head>");
+        res.write("<body><h1>Welcome to my Node Js project</h1></body>");
+        res.write("</html>");
+        res.end();
+    }
 });
+server.listen(4000);
