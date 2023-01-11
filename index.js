2c22c01bc982622eacfd941ddfea7f0b4b668501
deepakpatidar123 <118708892+deepakpatidar123@users.noreply.github.com>
2023-01-11 19:50:25 +0530

    debuge

--- index.js
+++ index.js
@@ -1,78 +1,28 @@
-const http = require("http");
-const fs=require('fs');
-const { buffer } = require("stream/consumers");
-const server = http.createServer((req, res) => {
-  const url = req.url;
-  const method= req.method;
-  const body=[];
-  if (url === "/") {
-    fs.readFile("message.txt",{encoding: "utf-8"},(err, data)=>{
-      if(err){
-        console.log(err);
-      }
-      res.write("<html>");
-      res.write("<head><title>Emter Message</title></head>");
-      res.write(`<body>${data}</body>`)
-      res.write(
-        '<body><form action="/message" method="POST"><input type="text" name="message"><button type="submit">sent</buttom></form></body>'
-        );
-        res.write('</html>');
-        return res.end();
-      })
-      }
-      else if(url==="/message" && method==="POST"){
-        req.on('data',(chunk)=>{
-        // console.log(chunk)
-        body.push(chunk)
-      })
+const http = require('http');
 
-      return req.on('end',()=>{
-        const parsedbody=Buffer.concat(body).toString();
-        // console.log(parsedbody)
-        const message=parsedbody.split('=')[1];
-        fs.writeFile('message.txt',message,(err)=>{
-          res.statusCode=302;
-          res.setHeader('Location','/')
-          // res.write('fs')
-          return res.end();
-        });
-      })
-  }
+const hostname = '127.0.0.1';
+const port = 3000;
+
+const server = http.createServer((req,res) =>{
+    res.statusCode = 200;
+    const url= req.url
+    res.setHeader('Content-Type','text/html');
+
+    if(url=='/home'){
+        res.write('Welcome Home');
+        res.end();
+       }
+    else if(url=='/about'){
+        res.write('Welcome to about us page');
+        res.end();
+        }
+    else if(url=='/node'){
+            res.write('Welcome to my node.js project');
+            res.end();
+            }
+    res.end();
+
+})
+server.listen(port, hostname , ()=>{
+    console.log(`server is running at http://${hostname}:${port}/`)
 });
-
-server.listen(5000);
-
-// const http=require("http")
-// const fs=require("fs")
-
-// const server=http.createServer((request, response)=>{
-//     if(request.url==="/"){
-//         response.end("HOME PAGE")
-//     }else if(request.url==="/data"){
-//      fs.readFile("./data.json",(err,data)=>{
-//         if(err){
-//             response.write(err)
-//             response.end()
-//         } else{
-//             response.end(data)
-//         }
-//      })
-     
-//     }else if(request.url==="/reports"){
-//         response.setHeader("Content-type", "text/html")
-//         response.end("<h2>Reports</h2>")
-    
-//     }else{
-//         response.end("Invalid End Point")
-//     }
-// })
-
-
-// server.listen(4500,()=>{
-//     console.log("The server is run at port 4500")
-// })
-    
-    
-    
-
-
