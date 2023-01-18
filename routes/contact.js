138b06151a7868856b403be25825a8d73b54662b
deepakpatidar123 <118708892+deepakpatidar123@users.noreply.github.com>
2023-01-18 13:14:11 +0530

    Serving HTML , CSS and JS (VIEWS)

--- routes/contact.js
+++ routes/contact.js
@@ -7,12 +7,12 @@ const rootDir = require('../util/path');
 const router = express.Router();
 
 // /admin/add-product => GET
-router.get('/add-product', (req, res, next) => {
-    res.sendFile(path.join(rootDir, 'views', 'add-product.html'));
+router.get('/contactus', (req, res, next) => {
+    res.sendFile(path.join(rootDir, 'views', 'contact.html'));
 });
 
 // /admin/add-product => POST
-router.post('/add-product',(req, res, next)=>{
+router.post('/contactus',(req, res, next)=>{
     console.log(req.body);
     res.redirect('/');
 });
