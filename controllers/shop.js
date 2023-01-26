a97f83c701df27292c6cd74a2c8367e9d15b84d9
deepakpatidar123 <118708892+deepakpatidar123@users.noreply.github.com>
2023-01-26 15:39:36 +0530

    adding a delete button in dynamic routes part 3

--- controllers/shop.js
+++ controllers/shop.js
@@ -40,11 +40,11 @@ exports.getCart = (req, res, next) => {
 };
 
 exports.postCart = (req, res, next) => {
-const prodId = req.body.productId;
-Product.findById(prodId, (product) =>{
-Cart.addProduct(prodId, product.price);
-})
-res.redirect('/cart');
+  const prodId = req.body.productId;
+  Product.findById(prodId, product => {
+    Cart.addProduct(prodId, product.price);
+  });
+  res.redirect('/cart');
 };
 
 exports.getOrders = (req, res, next) => {
